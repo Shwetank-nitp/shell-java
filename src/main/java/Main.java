af40150c9ad2311a7abef413d79b6d971637325c
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2026-06-11 19:25:04 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    private static void printCommandNotFoundErrorMessage(String command) {
+        String error = command+": command not found";
+        System.out.println(error);
+    }
+
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+
+        Scanner sc = new Scanner(System.in);
+
+        String command = sc.nextLine();
+
+        // print the error message
+        printCommandNotFoundErrorMessage(command);
     }
 }
