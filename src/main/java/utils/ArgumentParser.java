a94c862feefc03556cbe32b92fdcb0339f15e6e1
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2026-06-13 10:40:56 +0530

    codecrafters submit [skip ci]

--- src/main/java/utils/ArgumentParser.java
+++ src/main/java/utils/ArgumentParser.java
@@ -1,11 +1,26 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ArgumentParser {
+    private static HashSet<Character> special;
+
+    static {
+        special = new HashSet<>();
+
+        // set of special characters
+        special.add('$');
+        special.add('"');
+        special.add('\'');
+        special.add('`');
+        special.add('\\');
+        special.add('\n');
+    }
 
     public static String[] getArgs(String text) {
+        text = text.strip();
         List<String> tokens = new ArrayList<>();
 
         int i = 0;
@@ -22,6 +37,11 @@ public class ArgumentParser {
 
                     i++; // skip the quote
                     while (i < n && text.charAt(i) != quote) {
+                        if (quote == '"' && text.charAt(i) == '\\'
+                                && special.contains(text.charAt(i+1))
+                        ) {
+                            ++i;
+                        }
                         sb.append(text.charAt(i));
                         i++;
                     }
