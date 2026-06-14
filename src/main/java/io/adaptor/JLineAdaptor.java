9507cfa41b9dd2c85af9c927ee914b6b0b3979aa
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2026-06-14 20:30:24 +0530

    codecrafters submit [skip ci]

--- src/main/java/io/adaptor/JLineAdaptor.java
+++ src/main/java/io/adaptor/JLineAdaptor.java
@@ -1,16 +1,18 @@
 package io.adaptor;
 
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.*;
 import org.jline.reader.impl.DefaultParser;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JLineAdaptor implements IOAdapter {
-
+    private boolean tabFlag = false;
+    private String lastLookupWord = "";
     private final LineReader reader;
 
     public JLineAdaptor() throws IOException {
@@ -25,8 +27,67 @@ public class JLineAdaptor implements IOAdapter {
         reader = LineReaderBuilder.builder()
                 .terminal(terminal)
                 .parser(parser)
-                .completer(new ShellCompleter())
                 .build();
+
+        Completer completer = new ShellCompleter();
+
+        final Widget tabAutoCompletion = () -> {
+            Buffer buffer = reader.getBuffer();
+            String currBuffer = buffer.toString();
+            int cursor = buffer.cursor();
+
+            ParsedLine parsedLine = parser.parse(currBuffer, cursor, Parser.ParseContext.COMPLETE);
+            String currentWord = parsedLine.word();
+
+            List<Candidate> candidates = new ArrayList<>();
+            completer.complete(reader, parsedLine, candidates);
+
+            if (candidates.isEmpty()) {
+                tabFlag = false;
+                lastLookupWord = currentWord;
+                terminal.writer().write('\u0007');
+                terminal.flush();
+                return true;
+            }
+
+            if (!currentWord.equals(lastLookupWord)) {
+                tabFlag = false;
+            }
+            lastLookupWord = currentWord;
+
+            if (!tabFlag) {
+                if (candidates.size() == 1) {
+                    String candidate = candidates.getFirst().value();
+                    String suffix = candidate.substring(currentWord.length());
+                    buffer.write(suffix + " ");
+                    tabFlag = false;
+                } else {
+                    tabFlag = true;
+                    terminal.writer().write('\u0007');
+                    terminal.flush();
+                }
+            } else {
+                tabFlag = false;
+                terminal.writer().println();
+
+                for (Candidate c : candidates) {
+                    terminal.writer().print(c.value() + "  ");
+                }
+
+                terminal.writer().println();
+                terminal.flush();
+
+                reader.callWidget(LineReader.REDRAW_LINE);
+                reader.callWidget(LineReader.REDISPLAY);
+            }
+            return true;
+        };
+
+        reader.getWidgets().put("shell-tab", tabAutoCompletion);
+
+        reader.getKeyMaps()
+                .get(LineReader.MAIN)
+                .bind(new Reference("shell-tab"), "\t");
     }
 
     @Override
--- src/main/java/utils/CliTrie.java
+++ src/main/java/utils/CliTrie.java
@@ -7,14 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class CliTrie {
     private static class TrieNode {
         public boolean isFinish = false;
-        public TrieNode[] nodes = new TrieNode[226]; // 8bit trie for simple cli without Unicode
-
+        public HashMap<Character, TrieNode> nodes = new HashMap<>();
         public String word = null;
     }
 
@@ -54,12 +54,9 @@ public class CliTrie {
     public static void push(String word) {
         TrieNode curr = root;
 
-        for(char c : word.toCharArray()) {
-            if (curr.nodes[c] == null) {
-                curr.nodes[c] = new TrieNode();
-            }
-
-            curr = curr.nodes[c];
+        for (char c : word.toCharArray()) {
+            curr.nodes.computeIfAbsent(c, k -> new TrieNode());
+            curr = curr.nodes.get(c);
         }
 
         curr.isFinish = true;
@@ -70,12 +67,12 @@ public class CliTrie {
         TrieNode curr = root;
 
         for (char c : word.toCharArray()) {
-            if (curr.nodes[c] == null) {
+            if (!curr.nodes.containsKey(c)) {
                 return new ArrayList<>();
             }
-
-            curr = curr.nodes[c];
+            curr = curr.nodes.get(c);
         }
+
         List<String> ans = new ArrayList<>();
         dfs(curr, ans);
 
@@ -86,9 +83,8 @@ public class CliTrie {
         if (node == null) return;
         if (node.isFinish) ans.add(node.word);
 
-        for(int i = 0; i < node.nodes.length; i++) {
-            if (node.nodes[i] == null) continue;
-            dfs(node.nodes[i], ans);
+        for (TrieNode childNode : node.nodes.values()) {
+            dfs(childNode, ans);
         }
     }
 }
