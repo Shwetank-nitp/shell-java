5163c45c156b227177e814d9b1e5fa23cbc62148
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2026-06-19 17:35:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/command/JobsCommand.java
+++ src/main/java/command/JobsCommand.java
@@ -4,8 +4,11 @@ import data.CommandContext;
 import data.ShellProcess;
 import error.CommandRuntimeException;
 import error.InvalidCommand;
+import process.manager.ProcessManager;
 import process.manager.SimpleProcessManager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JobsCommand implements Command {
@@ -17,10 +20,30 @@ public class JobsCommand implements Command {
 
     @Override
     public ShellProcess execute() throws InvalidCommand, CommandRuntimeException {
-        List<ShellProcess> runningProcess = SimpleProcessManager
-                .getInstance()
-                .getRunningProcess();
-        // do the rest later
+        List<ShellProcess> jobs = SimpleProcessManager.getInstance().getRunningProcess();
+
+        for (int i = 0; i < jobs.size(); i++) {
+            ShellProcess p = jobs.get(i);
+
+            String marker;
+            if (i == 0) {
+                marker = "+";
+            } else if (i == 1) {
+                marker = "-";
+            } else {
+                marker = "";
+            }
+
+            String command = p.getContext().getCommandName();
+            if (p.getContext().getArgs().length > 0) {
+                command += " " + String.join(" ", p.getContext().getArgs());
+            }
+
+            String line = "[%d]%s  Running                 %s"
+                    .formatted(i + 1, marker, command);
+
+            System.out.println(line);
+        }
 
         return new ShellProcess(context, ProcessHandle.current().pid(), true, true);
     }
